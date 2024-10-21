342fa8800f4a8cf7f3335565c393d47a613211d8
Anjanikumar98 <120495869+Anjanikumar98@users.noreply.github.com>
2024-10-21 12:31:46 +0530

    Implement array

---
+++ Array_Problems/a4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class a4 {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.err.println("Enter the numbers of rows");
+        int rows = sc.nextInt();
+
+        System.err.println("Enter the numbers of columns");
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                System.err.print( numbers[i][j] + " ");
+            }
+            System.err.println(" ");
+        }
+    }
+}
