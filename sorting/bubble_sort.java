0f59e3de6676db9532b1371ba13bb966a0fedc6a
Anjanikumar98 <anjanikumar2314@gmail.com>
2025-07-29 14:39:17 +0530

    solving arrays question

--- sorting/bubble_sort.java
+++ sorting/bubble_sort.java
@@ -1,4 +1,5 @@
 public class bubble_sort {
+    
     static void bubbleSort(int[] arr, int n) {
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= i - 1; j++) {
--- sorting/insertion_sort.java
+++ sorting/insertion_sort.java
@@ -1,4 +1,5 @@
 public class insertion_sort {
+    
     static void insertionSort(int[] arr, int n) {
         for (int i = 0; i <= n - 1; i++) {
             int j = i;
